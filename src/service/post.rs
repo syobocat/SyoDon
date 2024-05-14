f826bda485bc71cc3e042b7c8e881fbca344cc28
SyoBoN <syobon@syobon.net>
2024-05-14 14:07:49 +0900

    implement deliver part

--- src/service/post.rs
+++ src/service/post.rs
@@ -1,8 +1,15 @@
+use std::collections::HashSet;
+
 use chrono::{SecondsFormat, Utc};
 use log::{error, info};
+use reqwest::Client;
 use rusqlite::Connection;
 use serde_json::json;
 use ulid::Ulid;
+use url::Url;
+
+use super::sign_header::create_header;
+use super::Method;
 
 pub async fn create(content: String) -> Result<(), Box<dyn std::error::Error>> {
     let config = crate::CONFIG.get().unwrap();
@@ -11,23 +18,31 @@ pub async fn create(content: String) -> Result<(), Box<dyn std::error::Error>> {
 
     let html = markdown::to_html(&content);
     let id = Ulid::new();
-    let time = Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true);
+    let time = Utc::now();
+    let time_str = time.to_rfc3339_opts(SecondsFormat::Secs, true);
 
     let conn = Connection::open(db).inspect_err(|e| error!("Failed to open database: {e}"))?;
     conn.execute(
         "INSERT INTO posts (id, content, date) VALUES (?1, ?2, ?3)",
-        (id.to_string(), &html, &time),
+        (id.to_string(), &html, &time_str),
     )
     .inspect_err(|e| error!("Failed to store post: {e}"))?;
 
     info!("Successfully stored note: {id}");
+
+    // Prioritize sharedInbox if exists
+    let mut stmt = conn.prepare("SELECT shared_inbox, inbox FROM followers")?;
+    let dests: HashSet<String> = stmt
+        .query_map([], |row| row.get(0).or(row.get(1)))?
+        .filter_map(|res| res.ok())
+        .collect();
 
     let body = json!({
         "@context": "https://www.w3.org/ns/activitystreams",
         "id": id.to_string(),
         "type": "Create",
         "actor": format!("{url}actor"),
-        "published": time,
+        "published": time_str,
         "to": ["https://www.w3.org/ns/activitystreams#Public"],
         "cc": [format!("{url}followers")],
         "object": {
@@ -36,13 +51,18 @@ pub async fn create(content: String) -> Result<(), Box<dyn std::error::Error>> {
             "attributedTo": format!("{url}profile"),
             "content": html,
             "url": format!("{url}post/{id}"),
-            "published": time,
+            "published": time_str,
             "to": ["https://www.w3.org/ns/activitystreams#Public"],
             "cc": [format!("{url}followers")]
         }
     });
 
-    // TODO: post this body
+    let client = Client::new();
+    for dest in dests {
+        let url: Url = dest.parse()?;
+        let header = create_header(Method::Post, &body, time, &url);
+        client.post(url).headers(header).json(&body).send().await?;
+    }
 
     Ok(())
 }
--- src/service/sign_header.rs
+++ src/service/sign_header.rs
@@ -1,13 +1,12 @@
-use std::collections::HashMap;
-
-use actix_web::http::Uri;
 use base64::{prelude::BASE64_STANDARD, Engine};
 use chrono::{DateTime, Utc};
+use reqwest::header::HeaderMap;
 use rsa::{
     pkcs1v15::SigningKey,
     sha2::{Digest, Sha256},
     signature::{SignatureEncoding, SignerMut},
 };
+use url::Url;
 
 use super::Method;
 
@@ -15,13 +14,13 @@ pub fn create_header(
     method: Method,
     body: &serde_json::Value,
     date: DateTime<Utc>,
-    dest: &Uri,
-) -> HashMap<String, String> {
+    dest: &Url,
+) -> HeaderMap {
     let config = crate::CONFIG.get().unwrap();
     let url = &config.server.url;
     let host = url.host().unwrap();
 
-    let dest_host = dest.host().unwrap_or_default();
+    let dest_host = dest.host().unwrap();
     let dest_path = dest.path();
 
     let date_rfc7231 = date.to_rfc2822().replace("+0000", "GMT");
@@ -29,7 +28,7 @@ pub fn create_header(
     let privkey = crate::PRIVKEY.get().unwrap().clone();
     let mut signingkey = SigningKey::<Sha256>::new(privkey);
 
-    let mut header = HashMap::new();
+    let mut header = HeaderMap::new();
 
     match method {
         Method::Get => {
@@ -38,10 +37,10 @@ pub fn create_header(
             let hashed_signature = signingkey.sign(&signature_string.into_bytes());
             let signature_base64 = BASE64_STANDARD.encode(hashed_signature.to_bytes());
             header.insert(
-                "Signature".to_owned(),
+                "Signature",
                 format!(
                     "keyId=\"{url}actor#main-key\",headers=\"(request-target) host date\",signature=\"{signature_base64}\""
-                ),
+                ).parse().unwrap(),
             );
         }
         Method::Post => {
@@ -53,22 +52,25 @@ pub fn create_header(
             );
             let hashed_signature = signingkey.sign(&signature_string.into_bytes());
             let signature_base64 = BASE64_STANDARD.encode(hashed_signature.to_bytes());
-            header.insert("Digest".to_owned(), format!("sha-256={digest_base64}"));
             header.insert(
-                "Signature".to_owned(),
-                format!(
-                    "keyId=\"{url}actor#main-key\",algorithm=\"rsa-sha256\",headers=\"(request-target) host date digest\",signature=\"{signature_base64}\""
-                ),
+                "Digest",
+                format!("sha-256={digest_base64}").parse().unwrap(),
             );
             header.insert(
-                "Content-Type".to_owned(),
-                "application/activity+json; charset=utf-8".to_owned(),
+                "Signature",
+                format!(
+                    "keyId=\"{url}actor#main-key\",algorithm=\"rsa-sha256\",headers=\"(request-target) host date digest\",signature=\"{signature_base64}\""
+                ).parse().unwrap(),
+            );
+            header.insert(
+                "Content-Type",
+                "application/activity+json; charset=utf-8".parse().unwrap(),
             );
         }
     }
 
-    header.insert("Host".to_owned(), dest_host.to_owned());
-    header.insert("Date".to_owned(), date_rfc7231);
+    header.insert("Host", dest_host.to_string().parse().unwrap());
+    header.insert("Date", date_rfc7231.parse().unwrap());
 
     header
 }
