bf764c2a52d9e741f3cd799807fdd0653b8192d6
SyoBoN <syobon@syobon.net>
2024-05-14 18:20:27 +0900

    switch from multipart/form to application/x-www-form-urlencoded

--- src/server/api/apps.rs
+++ src/server/api/apps.rs
@@ -1,26 +1,28 @@
-use actix_multipart::form::{text::Text, MultipartForm};
-use actix_web::{get, post, HttpResponse, Responder};
+use actix_web::{get, post, web::Form, HttpResponse, Responder};
 use base64::{prelude::BASE64_URL_SAFE_NO_PAD, Engine};
 use log::{error, info};
 use rand::{thread_rng, Rng, RngCore};
 use rusqlite::Connection;
+use serde::Deserialize;
 use serde_json::json;
 
-#[derive(MultipartForm)]
+// TODO: Support both application/x-www-form-urlencoded and multipart/form ?
+
+#[derive(Deserialize)]
 struct Application {
-    client_name: Text<String>,
-    redirect_uris: Text<String>,
-    _scopes: Option<Text<String>>, // Currently we don't use this
-    website: Option<Text<String>>,
+    client_name: String,
+    redirect_uris: String,
+    _scopes: Option<String>, // Currently we don't use this
+    website: Option<String>,
 }
 
 #[post("/api/v1/apps")]
-async fn apps(MultipartForm(form): MultipartForm<Application>) -> impl Responder {
+async fn apps(Form(form): Form<Application>) -> impl Responder {
     let config = crate::CONFIG.get().unwrap();
     let db = &config.server.db;
 
-    let client_name = form.client_name.into_inner();
-    let redirect_uris = form.redirect_uris.into_inner();
+    let client_name = form.client_name;
+    let redirect_uris = form.redirect_uris;
 
     let mut rng = thread_rng();
     let mut client_id = [0u8; 32];
@@ -50,7 +52,7 @@ async fn apps(MultipartForm(form): MultipartForm<Application>) -> impl Responder
     let body = json!({
         "id": rng.gen::<u16>().to_string(),
         "name": client_name,
-        "website": form.website.map(actix_multipart::form::text::Text::into_inner),
+        "website": form.website,
         "redirect_uri": redirect_uris,
         "client_id": client_id_base64,
         "client_secret": client_secret_base64
